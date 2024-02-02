28b8cae5fbef64e064dc900c92b29b5037fa7a39
MatthewShindel <matthew@shindel.com>
2024-02-01 20:44:12 -0500

    show movieInfo Co-authored-by:  Matthew Shindel : <matthew@shindel.com> Co-authored-by:  Dan Lavery : <lavery_william@yahoo.com>

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+import MovieInfo from '../MovieInfo/MovieInfo';
 import './Movie.css';
 
 
 function Movie({ id, posterPath, title, rating}){
+	const [movieInfo, setMovieInfo] = useState(false);
+	const onClick = () => setMovieInfo(true);
+
 	return (
-		<div className='movie-card' id={id}>
+		<div className='movie-card' id={id} onClick={onClick} >
 			<img src = {posterPath} alt = {title}/>
+			{ movieInfo ? <MovieInfo/> : null}
 			<h2>{rating}</h2>
 		</div>
 	)
